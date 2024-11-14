88a1289446417fa9b96c29f88e2dc56e3565d097
carlohcs <carlohcs@gmail.com>
2024-11-14 11:28:24 -0300

    feature:  adiciona unit tests para food.core.application.pagamento.adapters.controller

--- food-core/src/main/java/br/com/alfac/food/core/application/pagamento/adapters/controller/ControladorPagamento.java
+++ food-core/src/main/java/br/com/alfac/food/core/application/pagamento/adapters/controller/ControladorPagamento.java
@@ -1,6 +1,5 @@
 package br.com.alfac.food.core.application.pagamento.adapters.controller;
 
-import br.com.alfac.food.core.application.pagamento.adapters.gateways.RepositorioPagamentoGateway;
 import br.com.alfac.food.core.application.pagamento.adapters.presenter.PagamentoPresenter;
 import br.com.alfac.food.core.application.pagamento.dto.PagamentoResponse;
 import br.com.alfac.food.core.application.pagamento.usecases.ConsultarPagementoPorPedidoIdUseCase;
@@ -10,8 +9,8 @@ public class ControladorPagamento {
 
     private final ConsultarPagementoPorPedidoIdUseCase consultarPagementoPorPedidoIdUseCase;
 
-    public ControladorPagamento(final RepositorioPagamentoGateway pagamentoRepository) {
-        this.consultarPagementoPorPedidoIdUseCase = new ConsultarPagementoPorPedidoIdUseCase(pagamentoRepository);
+    public ControladorPagamento(final ConsultarPagementoPorPedidoIdUseCase consultarPagementoPorPedidoIdUseCase) {
+        this.consultarPagementoPorPedidoIdUseCase = consultarPagementoPorPedidoIdUseCase;
     }
 
     public PagamentoResponse consultarPagamentoPorPedidoId(Long pedidoId) throws FoodException {
